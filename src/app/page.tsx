21d32d99cf7d9df8ed117ffd4e18dced3f6bd7e0
Ryan M <129496054+Syyren@users.noreply.github.com>
2025-01-01 16:02:01 -0700

    experimenting with tailwind to achieve desired design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <h1>Hello World.</h1>
+function Greeting({ name }: { name: string }) {
+  return <h1 className="mx-auto">Hello, {name}</h1>;
+}
+
+export default function App() {
+  return <div className="container justify-center">
+    <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 max-w-med">
+      <Greeting name="Friend" />
+    </figure>
+  </div>
 }
